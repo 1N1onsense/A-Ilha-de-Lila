44b56c40acac6f2527dd729222092b55c59b1398
Christian Torres <53317790+christiantlira@users.noreply.github.com>
2026-01-11 10:05:10 -0300

    feat: Mecânica de diálogo adicionada ao jogo

--- Scripts/bixao_teste.gd
+++ Scripts/bixao_teste.gd
@@ -3,9 +3,16 @@ extends StaticBody2D
 @onready var interactable: Area2D = $Interactable
 @onready var sprite_2d: Sprite2D = $Sprite2D
 
+const lines: Array[String] = [
+	"Dale mano, meu nome é O Bichão Louco!",
+	"Alguns me chamam de mono Kha'zix.",
+	"Mas isso já faz muito tempo...",
+	"Eu já aposentei esse lado meu há muito tempo...",
+	"Mas cuidado, esse meu lado ainda habita dentro de mim."
+]
+
 func _ready() -> void:
 	interactable.interact = _on_interact
 
 func _on_interact():
-	sprite_2d.rotate(0.5)
-	print("O bixão rodô")
+	DialogManager.start_dialog(global_position, lines)
--- Scripts/interacting_component.gd
+++ Scripts/interacting_component.gd
@@ -15,20 +15,15 @@ func _input(event: InputEvent) -> void:
 			can_interact = true
 
 func _process(_delta: float) -> void:
-	print("1")
 	if current_interactions and can_interact:
-		print("2")
 		current_interactions.sort_custom(_sort_by_nearest)
 		if current_interactions[0].is_interactable:
-			print("3")
 			interact_label.text = current_interactions[0].interact_name
 			interact_label.show()
 	else:
-		print("4")
 		interact_label.hide()
 
 func _sort_by_nearest(area1, area2):
-	print("5")
 	var area1_dist = global_position.distance_to(area1.global_position)
 	var area2_dist = global_position.distance_to(area2.global_position)
 	print(area1_dist < area2_dist)
@@ -36,10 +31,8 @@ func _sort_by_nearest(area1, area2):
 
 # Adiciona interação ao entrar no range
 func _on_interact_range_area_entered(area: Area2D) -> void:
-	print("ELE ENTROU VI BOY")
 	current_interactions.push_back(area)
 
 # Remove interação ao sair do range
 func _on_interact_range_area_exited(area: Area2D) -> void:
-	print("agr saiu")
 	current_interactions.erase(area)
