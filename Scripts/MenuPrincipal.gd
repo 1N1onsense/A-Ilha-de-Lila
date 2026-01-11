28a8b60330fd6eb21a135a7dbb24e3e9dc00b9ab
RenattoPadilha <55454860+RenattoPadilha@users.noreply.github.com>
2026-01-11 18:11:26 -0300

    Update Menu Principal

--- Scripts/MenuPrincipal.gd
+++ Scripts/MenuPrincipal.gd
@@ -1,7 +1,8 @@
 extends Node
 
-func _on_iniciar_pressed() -> void:
+func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://Mapas e Cenas/mundo.tscn")
-	
-func _on_sair_pressed() -> void:
+	MusicManager.play()
+
+func _on_exit_pressed() -> void:
 	get_tree().quit()
