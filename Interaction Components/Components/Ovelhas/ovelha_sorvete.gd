0470ccd836be12d06bc6cd89b0a1d06644ef59d7
1N1onsense <77027667+1N1onsense@users.noreply.github.com>
2026-01-12 22:31:30 -0300

    pontos

--- Interaction Components/Components/Ovelhas/ovelha_sorvete.gd
+++ Interaction Components/Components/Ovelhas/ovelha_sorvete.gd
@@ -6,15 +6,15 @@ extends StaticBody2D
 const lines: Array[String] = [
 	"Oi! Grande pequena Lila!",
 	"Pode ver que os sorvetes aqui estão em fila?",
-	"Veja e se lembre da ordem que vão brilhar.",
+	"Veja e se lembre da ordem que vão brilhar",
 	"Faça isso três vezes e com uma vela vão te presentear!"
 ]
 
 const lines_venceu: Array[String] = [
 	"Lila, a peixinha gênia!",
-	"Estou admirada com sua memória.",
+	"Estou admirada com sua memória",
 	"Quando crescer, não se esqueça de mim!",
-	"Ou pelo menos lembre-se dessa história."
+	"Ou pelo menos lembre-se dessa história"
 ]
 
 func _ready() -> void:
